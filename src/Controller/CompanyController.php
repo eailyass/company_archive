8c0b80638e7fac9fcb0d4ca44c23b3f9db840109
ilyass.elassami <ilyass.elassami@external.engie.com>
2022-04-21 17:12:28 +0200

    add forms

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use App\Form\CompanyType;
+use App\Repository\CompanyRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/company', name: 'app_company_')]
+class CompanyController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $companies = $doctrine->getRepository(Company::class)->findAll();
+        return $this->render('company/index.html.twig', [
+            'companies' => $companies,
+        ]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CompanyRepository $repo): Response
+    {
+        $company = new Company();
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repo->add($company);
+        }
+        return $this->render('company/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/AddressType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number')
+            ->add('streetType')
+            ->add('streetName')
+            ->add('city')
+            ->add('zipCode')
+            ->add('company')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
