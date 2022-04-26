e465c2188b8f9e4fe15cd8d4ea6a1ad10ff55f3f
ilyass.elassami <ilyass.elassami@external.engie.com>
2022-04-26 17:46:02 +0200

    substitute entity listener by lifecycle subscriber

--- src/EventListener/SnapshotCompany.php
+++ src/EventListener/SnapshotCompany.php
@@ -2,20 +2,54 @@
 
 namespace App\EventListener;
 
+use App\Entity\Address;
 use App\Entity\Company;
+use Doctrine\ORM\Events;
 use App\Service\SnapshotService;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
 
-Class SnapshotCompany{
+Class SnapshotCompany implements EventSubscriberInterface{
 
     public function __construct(private SnapshotService $snapshot)
     {
         
     }
 
-    public function postUpdate(Company $company, LifecycleEventArgs $event): void
+    public function getSubscribedEvents(): array
     {
-        $this->snapshot->snapshotCompany($company);
+        return [
+            Events::postPersist,
+            Events::postUpdate,
+        ];
+    }
+    
+    public function postUpdate( LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof Company) {
+            $this->snapshot->snapshotCompany($entity);
+            return;
+        }
+        if ($entity instanceof Address) {
+            $this->snapshot->snapshotCompany($entity->getCompany());
+            return;
+        }
+    }
+
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof Company) {
+            $this->snapshot->snapshotCompany($entity);
+            return;
+        }
+        if ($entity instanceof Address) {
+            $this->snapshot->snapshotCompany($entity->getCompany());
+            return;
+        }
     }
 
 }
